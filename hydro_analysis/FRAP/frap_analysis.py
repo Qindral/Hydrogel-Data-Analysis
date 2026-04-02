81284eb962e214f103d7e10ef0fdfafb29b82ffc
Qindral <104197453+Qindral@users.noreply.github.com>
2026-04-02 19:14:31 +0200

    Update frap_analysis.py

--- hydro_analysis/FRAP/frap_analysis.py
+++ hydro_analysis/FRAP/frap_analysis.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 # =============================================================================
 # Configuration
 # =============================================================================
-BASE_DIR = Path(r"H:\Daten Promotion Sicherung\Confocal_Measure\2025_09_08_14_57_46--Project_TIF\Water")
+BASE_DIR = Path(r"H:\Daten Promotion Sicherung\Confocal_Measure\2025_09_08_14_57_46--Project_TIF\Water 2")
 
 # =============================================================================
 # 1. Discover subfolders (Pre / Pb)
